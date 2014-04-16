78970204daa63e5157c21b919e99128b4dd8e714
RobHawk <robert@escolasimples.com.br>
2014-04-16 15:38:04 -0300

    login simplified

--- src/main/java/com/example/services/UsuarioServices.java
+++ src/main/java/com/example/services/UsuarioServices.java
@@ -1,11 +1,6 @@
 package com.example.services;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,6 +11,7 @@ import com.example.models.Usuario;
 
 @Path("/usuario")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UsuarioServices {
 
 	/*
@@ -37,23 +33,9 @@ public class UsuarioServices {
 	@POST
 	@Path("/login")
 	public Response login(Usuario usuario) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("test");
-		EntityManager manager = factory.createEntityManager();
-		EntityTransaction transaction = manager.getTransaction();
-
-		transaction.begin();
-
-		TypedQuery<Usuario> query = manager.createQuery("select u from Usuario u where u.nome = " + usuario.getNome()
-				+ " and u.senha = " + usuario.getSenha(), Usuario.class);
-
-		transaction.commit();
-
-		Usuario master = query.getResultList().get(0);
-
-		if (master != null)
-			return Response.ok().build();
+		if (usuario.getNome().equals("master") && usuario.getSenha().equals("padrao"))
+			return Response.ok().entity(usuario).build();
 		else
-			return Response.noContent().build();
-
+			return Response.status(401).entity(usuario).build();
 	}
 }
